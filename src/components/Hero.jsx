df1704d7801265499700e5679cefd8fc14b4bac7
kenmikanmi <kenh5526@gmail.com>
2020-06-28 17:41:36 +0900

    Modify Hero compoentn

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,15 @@ import React from "react"
 const Hero = () => (
   <div className="container py-12 lg:pb-16">
     <h2 className="text-3xl sm:text-4xl font-extrabold leading-tight tracking-tight text-gray-900">
-      Hello, I'm John{" "}
+      Artworks{" "}
       <span role="img" aria-label="waving hand">
-        👋
+        🎨
       </span>
       <br />
-      <span className="text-blue-600">
-        Welcome to my photography portfolio.
-      </span>
     </h2>
+    <p className="text-blue-700">
+        作った作品や撮った写真などを貼っていきます。
+    </p>
   </div>
 )
 
